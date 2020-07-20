7b4f5cfb1259be0fac35233adff57ec291c532d1
aarif4 <aarif4@masonlive.gmu.edu>
2020-07-20 00:38:10 -0400

    implemented new writer interface but having problems referencing header files from reckless

---
+++ inc/haste_writer.hpp
@@ -0,0 +1,34 @@
+#ifndef HASTE_WRITER_HPP
+#define HASTE_WRITER_HPP
+
+#include <reckless>
+
+namespace haste {
+
+class haste_writer : public writer {
+public:
+#if defined(__unix__)
+    haste_writer(int fd, bool stderr_fd) : fd_(fd), stderr_(stderr_fd) {}
+#elif defined(_WIN32)
+    haste_writer(void* handle, bool stderr_fd) : handle_(handle), stderr_(stderr_fd) {}
+#endif
+
+    std::size_t write(void const* pbuffer, std::size_t count, std::error_code& ec) noexcept override;
+
+#if defined(__unix__)
+    int fd_;
+    int stderr_;
+#elif defined(_WIN32)
+    void* handle_;
+    void* stderr_;
+#endif
+
+private:
+    std::size_t write_to_file(void const* pbuffer, std::size_t count, std::error_code& ec) noexcept override;
+    std::size_t write_to_stderr(void const* pbuffer, std::size_t count, std::error_code& ec) noexcept override;
+
+};
+
+}   // namespace haste
+
+#endif  // HASTE_WRITER_HPP
